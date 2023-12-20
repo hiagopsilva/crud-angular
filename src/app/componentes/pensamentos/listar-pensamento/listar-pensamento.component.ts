3bb9cf5456cd19f08174b92d135a488979480709
Hiago Prates <hiagohps360@gmail.com>
2023-12-19 21:52:44 -0300

    feat: ngclass

--- src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
+++ src/app/componentes/pensamentos/listar-pensamento/listar-pensamento.component.ts
@@ -7,6 +7,23 @@ import { Component } from '@angular/core';
 })
 export class ListarPensamentoComponent {
   listaPensamentos = [
-
+    {
+      id: '1',
+      conteudo: 'Aprendendo Angular',
+      autoria: 'DEV',
+      modelo: 'modelo1'
+    },
+    {
+      id: '2',
+      conteudo: 'Aprendendo Angular 2',
+      autoria: 'DEV',
+      modelo: 'modelo2'
+    },
+    {
+      id: '2',
+      conteudo: ',Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3Aprendendo Angular 3',
+      autoria: 'DEV',
+      modelo: 'modelo3'
+    }
   ];
 }
