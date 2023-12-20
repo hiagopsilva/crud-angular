0d5c71ace801ce5da87fd55c11846e70e2b4391e
Hiago Prates <hiagohps360@gmail.com>
2023-12-19 22:40:28 -0300

    feat: config form group criar pensamento

--- src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { PensamentoService } from '../pensamento.service';
 import { IPensamento } from '../pensamento/interface.pensamento';
 import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -10,20 +11,25 @@ import { Router } from '@angular/router';
 })
 export class CriarPensamentoComponent {
 
-  pensamento: IPensamento = {
-    conteudo: '',
-    autoria: '',
-    modelo: ''
-  }
+  formulario!: FormGroup;
 
   constructor(
     private service: PensamentoService,
-    private router: Router
+    private router: Router,
+    private formBuilder: FormBuilder
   ) {
   }
 
+  ngOnInit(): void {
+    this.formulario = this.formBuilder.group({
+      conteudo: ['Formulário de pensamento'],
+      autoria: ['cascsac'],
+      modelo: ['modelo de pensamento']
+    });
+  }
+
   criarPensamento() {
-    this.service.criar(this.pensamento).subscribe()
+    this.service.criar(this.formulario.value).subscribe()
 
     this.router.navigate(['/listarPensamento'])
   }
