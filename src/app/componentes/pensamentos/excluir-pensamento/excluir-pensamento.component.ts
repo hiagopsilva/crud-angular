d619286115271020288ee45f64b0d76036c2f802
Hiago Prates <hiagohps360@gmail.com>
2023-12-19 22:22:11 -0300

    feat: component excluir pensamento

---
+++ src/app/componentes/pensamentos/excluir-pensamento/excluir-pensamento.component.ts
@@ -0,0 +1,45 @@
+import { Component } from '@angular/core';
+import { IPensamento } from '../pensamento/interface.pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { ActivatedRoute, Router } from '@angular/router';
+
+@Component({
+  selector: 'app-excluir-pensamento',
+  templateUrl: './excluir-pensamento.component.html',
+  styleUrls: ['./excluir-pensamento.component.css']
+})
+export class ExcluirPensamentoComponent {
+
+  pensamento: IPensamento = {
+    id: 0,
+    conteudo: '',
+    modelo: '',
+    autoria: ''
+  }
+
+  constructor(
+    private service: PensamentoService,
+    private router: Router,
+    private route: ActivatedRoute
+  ) { }
+
+  ngOnInit(): void {
+    const id = this.route.snapshot.paramMap.get('id')
+
+    this.service.buscarPorId(Number(id)).subscribe(pensamento => {
+      this.pensamento = pensamento
+    })
+  }
+
+  excluirPensamento() {
+    if (this.pensamento.id) {
+      this.service.excluir(this.pensamento.id ).subscribe()
+
+      this.router.navigate(['/listarPensamento'])
+    }
+  }
+
+  cancelar() {
+    this.router.navigate(['/listarPensamento'])
+  }
+}
